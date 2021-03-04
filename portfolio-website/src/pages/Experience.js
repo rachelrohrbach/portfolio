233c6851cd565709a09438fc82447f40b95c3678
Rachel Rohrbach <rachel.rohrbach@gmail.com>
2021-03-04 11:23:57 -0800

    Copy files

---
+++ portfolio-website/src/pages/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Skills from '../components/Skills';
+
+function Experience() {
+  return (
+    <div className="skills-resume">
+      <Skills />
+    </div>
+  );
+}
+
+export default Experience;
---
+++ portfolio-website/src/pages/Projects.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import Col from '../components/Col';
+import { Col, Container, Row } from 'react-bootstrap';
+import projects from '../projectsData';
+import ProjectCard from '../components/ProjectCard';
+
+function Projects() {
+
+  console.log();
+
+  return (
+    <div>
+      <Container className='pt-5'>
+        <h1 className='pb-3'> Projects </h1>
+        <Row>
+          {projects.map(project => (
+            <Col size='sm-12 md-6 lg-4' key={project.name}>
+              <ProjectCard
+                name={project.name}
+                description={project.description}
+                imgUrl={project.imgUrl}
+                github={project.github}
+                link={project.link}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Projects;
